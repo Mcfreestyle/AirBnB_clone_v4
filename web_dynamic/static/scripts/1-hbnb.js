3968257a47993091860dd33d093d5b2c90702dd8
Mcfreestyle <mcfreestyle.go1@gmail.com>
2022-05-28 18:09:52 -0500

    feat: Listen for change on each input checkbox tag and update the h4 tag inside the div Amenities with the list of Amenities checked

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-      $("#amenity_id").click(function() {
-        alert("Hello world!");
-      });
-});
+// Listen for change on each input checkbox tag and update the h4 tag inside the div Amenities with the list of Amenities checked
+$(document).ready(getChecks);
+
+function getChecks () {
+  $('.amenities input').change(function () {
+    const amenities = {};
+    $('.amenities input:checked').each(function () {
+      amenities[$(this).data('id')] = $(this).data('name');
+    });
+    const names = Object.values(amenities).join(', ');
+    $('.amenities h4').text(names);
+  });
+}
